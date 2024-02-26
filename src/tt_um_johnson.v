8e6505c1a0cda8039191bf511e1e22ecc5680263
Xianshun Song <152236102+XianshunSong@users.noreply.github.com>
2024-02-26 18:34:11 -0500

    Update tt_um_johnson.v

--- src/tt_um_johnson.v
+++ src/tt_um_johnson.v
@@ -1,9 +1,10 @@
 module tt_um_johnson (    
     input  wire [7:0] ui_in,    // Dedicated inputs
-    output reg [7:0] out,   // Dedicated outputs
+    output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,
+    output reg  [7:0] out,
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n,     // reset_n - low to reset
