4e7bb5d38b1938ab6a6c8d1c8ac6c33e322e533d
Xianshun Song <152236102+XianshunSong@users.noreply.github.com>
2024-02-26 18:25:22 -0500

    Update tt_um_johnson.v

--- src/tt_um_johnson.v
+++ src/tt_um_johnson.v
@@ -1,9 +1,9 @@
-module tt_um_johnson (clk, r, ena, out);
+module tt_um_johnson (clk, r, ena, rst_n, out);
 
 parameter size =7;
 input clk;
 input r;
-input ena;
+input ena, rst_n;
 output [0:size] out;
 
 reg [0:size] out;
