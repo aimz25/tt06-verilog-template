09a4dcf054d65ee5ddd4b903ea6abfee9d94027f
Xianshun Song <152236102+XianshunSong@users.noreply.github.com>
2024-02-26 18:47:34 -0500

    Update tt_um_johnson.v

--- src/tt_um_johnson.v
+++ src/tt_um_johnson.v
@@ -1,6 +1,6 @@
 module tt_um_johnson (    
     input  wire [7:0] ui_in,    // Dedicated inputs
-    output reg  [7:0] uo_out,   // Dedicated outputs
+    output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,
